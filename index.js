69965f97c78608296274fe9d48d39ede0f67dc13
Devon4352 <devonbeau@icloud.com>
2025-03-11 13:53:28 +0000

    Update

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors =require('cors');
 app.use(bodyParser.json());
 app.use(express.json());
 const{ PrismaClient}= require ("@prisma/client");
 const { response } = require('express');
 const prisma = new PrismaClient();
 const port = 3000
-app.get('/notes/all',async (req,res) =>{
+app.use(cors({
+    origin: 'http://localhost:80', 
+}));
+app.get('/',async (req,res) =>{
     const allNotes = await prisma.note.findMany();
     res.status(200).json({
         allNotes
@@ -15,41 +19,53 @@ app.get('/notes/all',async (req,res) =>{
     console.log(allNotes);
     return;
     })
-app.get('/notes',async(req,res)=>{
+app.get('/get',async(req,res)=>{
     const {id} = req.body.Note;
-    
-    const note = await prisma.note.findUnique(
-    {where:{
-        
-        id:parseInt(id),
-        }
-    })
-    res.status(200).json({
-        note
-    });
-    console.log(note);
+    if(!id == null){
+        const note = await prisma.note.findUnique(
+        {where:{
+            
+            id:parseInt(id),
+            }
+        })
+        res.status(200).json({
+            note
+        });
+        console.log(note);
+    }
+    else
+    {
+        const allNotes = await prisma.note.findMany();
+        res.status(200).json({
+            allNotes
+        });
+        console.log(allNotes);
+        return;
+    }
 })
-app.post('/notes', async (req, res) => {
+app.post('/add', async (req, res) => {
     console.log(req.body);
   const{title,body,date} = req.body.Note;
-  //const currentdate = new Date()
-  //var title ='title';
-  //var body = 'body';
-  //var date = (currentdate.getDate())+'/'+(currentdate.getMonth()+1);
-  new_Note(title,body,date);
+  if(!title||!body||!date)
+    {
+        res.status(200).json('All fields must be filled');
+        console.log('Error: Missing field')
+        return;
+    }
+    new_Note(title,body,date);
   const allNotes = await prisma.note.findMany();
     res.status(200).json({
         allNotes
     });
     console.log(allNotes);
 })
-app.delete('/notes/clear',async(req,res) => {
+app.delete('/clear',async(req,res) => {
     const del = await prisma.note.deleteMany();
     res.status(200).json({
         del
     });
 })
-app.delete('/notes',async(req,res) =>{
+app.delete('/delete',async(req,res) =>{
    const{ id } = req.body.Note;
     const del = await prisma.note.delete({
         where: {id:parseInt(id)}
@@ -59,8 +75,14 @@ app.delete('/notes',async(req,res) =>{
     });
     console.log(del);
 })
-app.patch('/notes', async(req,res)=>{
+app.patch('/update', async(req,res)=>{
 const{id,title,body,date} = req.body.Note;
+if(!id)
+    {
+        res.status(200).json('ID must be filled');
+        console.log('Error: Missing ID')
+        return;
+    }
 const edit = await prisma.note.update({
     where:{
         id : parseInt(id),
